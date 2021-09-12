5f78d80b449fb1ff7c20cd8797c2f5d0b13a1e01
marvinweber <marvin@marvinweber.net>
2021-09-12 13:32:36 +0200

    Cleanup

--- example_usage/stable_baselines_test.py
+++ example_usage/stable_baselines_test.py
@@ -1,21 +1,22 @@
-import gym
+#
+# Example Script: Train PPO policy with stable baselines against Gym Environment
+#
 
+import gym
 from stable_baselines3 import A2C, PPO
+
 
 env = gym.make('Acrobot-v1')
 
 # model = transitions/Environment without rewards/done and visualization
-model = PPO('MlpPolicy', env, verbose=1) #A2C('MlpPolicy', env, verbose=1)
+model = PPO('MlpPolicy', env, verbose=1)
+#model = A2C('MlpPolicy', env, verbose=1)
 model.learn(total_timesteps=100000)
 
 obs = env.reset()
-# framework_env = gym.make('Acrobot-v1', model)
-# obs = framework_env.reset()
 for i in range(1000):
     action, _state = model.predict(obs, deterministic=True)
-    
-    # obs, reward, done, info = framework_env.step(action)
     obs, reward, done, info = env.step(action)
     env.render()
     if done:
-      obs = env.reset()
+        obs = env.reset()
