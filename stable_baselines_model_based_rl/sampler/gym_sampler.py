bd5af1e7abf5c5ae452fde851a885a482d4309d9
marvinweber <marvin@marvinweber.net>
2021-09-17 12:10:07 +0200

    Improve sampling performance by directly writing to CSV file
    This improved performance by ~ 20x.
    Example samping of "MountainCarContinuous-v0" environment took
    arround 279 seconds with old version, now only arround 12 seconds.

--- stable_baselines_model_based_rl/sampler/gym_sampler.py
+++ stable_baselines_model_based_rl/sampler/gym_sampler.py
@@ -1,8 +1,8 @@
+import csv
 import datetime
 import os
 
 import gym
-import pandas as pd
 from gym.spaces import space
 from gym.spaces.box import Box
 from gym.spaces.discrete import Discrete
@@ -41,7 +41,10 @@ def __update_observation_input_config(config: Configuration, observation_cols, o
                    [float(x) for x in list(obs_space.high)])
 
 
-def __sample_gym_environment(gym_environment_name: str, episode_count=20, max_steps=100):
+def __sample_gym_environment(gym_environment_name: str, data_file: str, episode_count=20,
+                             max_steps=100):
+    """Sample given gym environment, create proper config and store generated data in data_file."""
+
     config = Configuration(os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                         '../../example_usage/sample_config.yaml'))
     env = gym.make(gym_environment_name)
@@ -57,12 +60,11 @@ def __sample_gym_environment(gym_environment_name: str, episode_count=20, max_st
     __update_observation_input_config(config, observation_col_names, env.observation_space)
     config.set('gym_sampling.gym_environment_name', gym_environment_name)
 
-    # PREPARE DATA FRAME
-    df = pd.DataFrame(data={
-        'EPISODE': [],
-        'STEP': [],
-        **{col: [] for col in action_col_names + observation_col_names},
-    })
+    data_file_handle = open(data_file, mode='w', encoding='UTF-8', newline='')
+    csv_writer = csv.writer(data_file_handle, delimiter=',')
+
+    # CSV Header
+    csv_writer.writerow(['EPISODE', 'STEP', *action_col_names, *observation_col_names])
 
     # SAMPLE DATA
     for episode in range(episode_count):
@@ -77,19 +79,16 @@ def __sample_gym_environment(gym_environment_name: str, episode_count=20, max_st
             action_sv = space_value_from_gym(env.action_space, action, SpaceType.ACTION)
             obs_sv = space_value_from_gym(env.observation_space, obs)
 
-            df = df.append({
-                'EPISODE': int(episode),
-                'STEP': int(step),
-                **action_sv.to_column_dict(),
-                **obs_sv.to_column_dict(),
-            }, ignore_index=True)
+            # Append row to CSV file
+            csv_writer.writerow([int(episode), int(step), *action_sv.to_value_list(),
+                                 *obs_sv.to_value_list()])
 
             obs, reward, done, _ = env.step(action)
 
         print('  --> finished after %d steps' % step)
 
-    df = df.astype({'EPISODE': int, 'STEP': int})
-    return df, config
+    data_file_handle.close()
+    return config
 
 
 def sample_gym_environment(gym_environment_name: str, episode_count=20, max_steps=100,
@@ -103,24 +102,29 @@ def sample_gym_environment(gym_environment_name: str, episode_count=20, max_step
       - A YAML file, containing the configuration that results from the sampled gym
         environment, based on the sample_config.yaml file.
 
+    Both files are stored within the output_path directory. They will be subfolders of directories
+    containing the gym environment name and the current time. E.g.
+    "CartPole-v1/2021-05-01-10-00-30/data.csv".
+
     Args:
         gym_environment_name: Name of the Gym-Environment to sample.
         epsiode_count: Amount of episodes to use for the sampling.
         max_steps: Maximum steps per episode allowed during sampling.
-        output_path: The directory the generated files get saved in
+        output_path: The directory the generated files are stored in.
 
-    Returns (path_to_csv_data_file, configuration object)
+    Returns:
+        data_file: Path to the created data (csv) file.
+        config: Configuration object created for the sampled environment.
     """
-
-    df, config = __sample_gym_environment(gym_environment_name, episode_count=episode_count,
-                                          max_steps=max_steps)
 
     final_dir_name = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
     final_dir_path = os.path.join(output_path, gym_environment_name, final_dir_name)
     os.makedirs(final_dir_path)
 
     data_file = f'{final_dir_path}/data.csv'
-    df.to_csv(data_file, sep=',', encoding='utf-8', index=False)
+    config = __sample_gym_environment(gym_environment_name, data_file, episode_count=episode_count,
+                                      max_steps=max_steps)
+
     config.set('input_config.input_file_name', os.path.abspath(data_file))
     config.set('model_wrapping.reset.data_file', os.path.abspath(data_file))
     config.set('model_wrapping.reset.type', 'EPISODE_START')
